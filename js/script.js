96b5b1358ee2c0ef6bca8fb2aaa41713dfef3924
Michele Prova <michele.prova99@gmail.com>
2020-07-09 09:43:43 +0200

    fine

--- js/script.js
+++ js/script.js
@@ -1,15 +1,33 @@
-var email = prompt ("Inserisci la tua email");
-var lista = ["uno@gmail.com", "due@gmail.com", "tre@gmail.com", "quattro@gmail.com", "cinque@gmail.com"];
-var accesso = false;
+// var email = prompt ("Inserisci la tua email");
+// var lista = ["uno@gmail.com", "due@gmail.com", "tre@gmail.com", "quattro@gmail.com", "cinque@gmail.com"];
+// var accesso = false;
+//
+// for (var i = 0; i < lista.lenght; i++){
+//   if (email == lista[i]) {
+//     accesso = true;
+//   }
+// }
+//
+// if (accesso == true){
+//   alert("Benvenuto!");
+// } else {
+//   alert("email non trovata");
+// }
 
-for (var i = 0; i < lista.lenght; i++){
-  if (email == lista[i]) {
-    accesso = true;
-  }
+var mail = prompt ("inserisci la tua email")
+var mailList = ["uno@gmail.com", "due@gmail.com", "tre@gmail.com", "quattro@gmail.com", "cinque@gmail.com"]
+
+var trovata = false;
+
+for (var i = 0; i < mailList.length; i++) {
+    if (mail == mailList [i]) {
+        trovata=true;
+    }
 }
 
-if (accesso == true){
-  alert("Benvenuto!");
+if (trovata == true) {
+    alert ("Benvenuto!")
+
 } else {
-  alert("email non trovata");
+    alert ( "email non trovata :(")
 }
